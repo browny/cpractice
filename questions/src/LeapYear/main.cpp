de1648e73608d70b7223b3b58d2bbfc9d7ed3eb4
Browny Lin <browny_lin@htc.com>
2013-03-15 11:55:24 +0800

    fix leap year

--- questions/src/LeapYear/main.cpp
+++ questions/src/LeapYear/main.cpp
@@ -7,9 +7,7 @@ int main()
 	int year;
 	cout << "Please enter a year:" << endl;
 	while (cin >> year) {
-		if( (year%4 == 0) && ( year%100 != 0))
-			cout << "year " << year << " is leap year" << endl;
-		else if((year%400==0) && (year%100==0))
+		if( (year%4)==0 && (year%100)!=0 || (year%400)==0 )
 			cout << "year " << year << " is leap year" << endl;
 		else
 			cout << "year" << year << " is not leap year" << endl;
