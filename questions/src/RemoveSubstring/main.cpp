f364ebf92596c5c989ecf3b948f1b90fea8ef4ee
Browny Lin <browny_lin@htc.com>
2013-03-15 16:33:06 +0800

    Add remove substring from string

---
+++ questions/src/RemoveSubstring/main.cpp
@@ -0,0 +1,37 @@
+
+#include <string.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string mainString, subString;
+
+	cout << "enter a string" << endl;
+	getline (cin, mainString);
+
+	cout << "enter a word to be removed" << endl;
+	getline (cin, subString);
+
+	cout << "the removed string:" << endl;
+
+	for (int i = 0; i < mainString.length(); ++i) {
+		int mp = i;
+		int sp = 0;
+		while (mainString[mp] == subString[sp] &&
+				sp < subString.length() && mp < mainString.length()) {
+			mp++;
+			sp++;
+		}
+		if (sp == 0)
+			cout << mainString[i];
+		else
+			i = i + sp - 1;
+	}
+	cout << endl;
+
+	return 0;
+}
+
