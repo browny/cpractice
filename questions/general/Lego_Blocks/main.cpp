24b269c2ccbf6a2d5d2ca96e3ed44fbacea2e81e
Browny Lin <browny_lin@htc.com>
2013-03-30 09:53:09 +0800

    refactor

---
+++ questions/general/Lego_Blocks/main.cpp
@@ -0,0 +1,47 @@
+/*
+ *  You have 4 types of lego blocks, of sizes 1 * 1 * 1, 1 * 1 * 2, 1 * 1 * 3 and 1 * 1 * 4. 
+ *  Assume you have infinite number of blocks of each type.
+ *  
+ *  You want to make a wall of height N and width M out of these blocks.
+ *  The wall should not have any holes in it. The wall you build should be one solid 
+ *  structure. A solid structure means that it should not be possible to separate 
+ *  the wall along any vertical line without cutting any lego block used to build the wall. 
+ *  The blocks can only be placed horizontally. In how many ways can the wall be built?
+ *  Ref: http://www.hackerrank.com/challenges/lego-blocks
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int cnt = 0;
+int lego[] = {1, 2, 3};
+vector<int> subset(lego, lego+3);
+
+void loop(int n) {
+	if (n > 0) {
+		vector<int> arr(subset.size(), n);
+		for (int i = 0; i < subset.size(); i++) {
+			arr[i] = arr[i] - subset[i];
+			if (arr[i] == 0)
+				cnt++;
+			loop(arr[i]);
+		}
+	} else {
+		return;
+	}
+}
+
+int main() {
+	int M = 4;
+	int M1 = loop(1);
+	int M2 = loop(2);
+	int M3 = loop(3);
+
+	cout << M1 << " " << M2 << " " << M3 << endl;
+
+	return 0;
+}
