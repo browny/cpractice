7ad3efc9a1de2b4ce9c4ff4826e048090f0a9405
michael <michael@mmics.(none)>
2013-04-01 19:37:57 +0800

    1's complement: fix tab

--- amazon/one_complement/main.cpp
+++ amazon/one_complement/main.cpp
@@ -6,15 +6,15 @@ int getIntergerComplement(int n)
 {
     int bits[32] = {0};
     int len = 0;
-    
+
     if (n == 0)
         return 1;
-        
+
     while (n > 0) {
         bits[len++] = n % 2 ? 0 : 1;
         n >>= 1;
     }
-    
+
     int result = 0;
     for (int i = len - 1; i >= 0; i--) {
         n = n + bits[i] * pow(2, i);
